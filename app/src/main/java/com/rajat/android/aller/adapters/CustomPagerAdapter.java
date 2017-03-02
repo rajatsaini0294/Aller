7ad09dc4c6ef9d52fb129c7a767d69b3ebde05eb
rks0294 <rk.saini0294@gmail.com>
2017-03-02 11:10:33 +0530

    view pager with tablayout added

---
+++ app/src/main/java/com/rajat/android/aller/adapters/CustomPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.rajat.android.aller.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.rajat.android.aller.ui.fragments.FavouriteLocationsFragment;
+import com.rajat.android.aller.ui.fragments.FutureLocationsFragment;
+import com.rajat.android.aller.ui.fragments.VisitedLocationsFragment;
+
+/**
+ * Created by rajat on 3/2/2017.
+ */
+public class CustomPagerAdapter extends FragmentPagerAdapter{
+
+    public CustomPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0: return new VisitedLocationsFragment();
+            case 1: return new FavouriteLocationsFragment();
+            case 2: return new FutureLocationsFragment();
+            default: return new VisitedLocationsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0: return "Visited";
+            case 1: return "Favourite";
+            case 2: return "To visit";
+            default: return "Visited";
+        }
+    }
+}
---
+++ app/src/main/java/com/rajat/android/aller/ui/activities/MainActivity.java
@@ -0,0 +1,29 @@
+package com.rajat.android.aller.ui.activities;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.rajat.android.aller.R;
+import com.rajat.android.aller.adapters.CustomPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        CustomPagerAdapter customPagerAdapter = new CustomPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(customPagerAdapter);
+
+        TabLayout tabLayout =  (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}

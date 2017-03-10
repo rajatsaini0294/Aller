b6ff75158d8809db56a51846ca23cc1baba46c34
rks0294 <rk.saini0294@gmail.com>
2017-03-10 20:41:03 +0530

    added details in locationdetailsactivity

--- app/src/main/java/com/rajat/android/aller/ui/activities/LocationDetailsActivity.java
+++ app/src/main/java/com/rajat/android/aller/ui/activities/LocationDetailsActivity.java
@@ -2,8 +2,10 @@ package com.rajat.android.aller.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.rajat.android.aller.R;
@@ -13,9 +15,15 @@ public class LocationDetailsActivity extends AppCompatActivity {
     final private String PARCEL_KEY = "parcelKey";
 
     LocationPOJO locationPOJO;
-    TextView textView;
+    TextView placeNameTextView;
     Toolbar toolbar;
+
     String placeName;
+    String placeAddress;
+
+    CardView cardView;
+    ImageView placeImage;
+    TextView placeAddressTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +31,24 @@ public class LocationDetailsActivity extends AppCompatActivity {
         locationPOJO = getIntent().getParcelableExtra(PARCEL_KEY);
         setContentView(R.layout.activity_location_details);
 
-        textView = (TextView) findViewById(R.id.place_name);
+        placeNameTextView = (TextView) findViewById(R.id.place_name);
+        cardView = (CardView) findViewById(R.id.card_view);
+        placeImage = (ImageView) findViewById(R.id.place_image);
+        placeAddressTextView = (TextView) findViewById(R.id.place_address);
 
         placeName = locationPOJO.getPlace_name();
-        textView.setText(placeName);
+        placeNameTextView.setText(placeName);
+
+        placeAddress = locationPOJO.getPlace_address();
+        if (placeAddress == null) {
+            placeAddressTextView.setVisibility(View.GONE);
+        } else {
+            placeAddressTextView.setText(placeAddress);
+        }
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(placeName);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -41,6 +60,6 @@ public class LocationDetailsActivity extends AppCompatActivity {
             }
         });
 
-        getSupportActionBar().setTitle(placeName);
+
     }
 }

5b596a410c12cd1a614fa5c85c2802050260bdf7
rks0294 <rk.saini0294@gmail.com>
2017-03-10 14:10:47 +0530

    added logic to locationPOJO

--- app/src/main/java/com/rajat/android/aller/model/LocationPOJO.java
+++ app/src/main/java/com/rajat/android/aller/model/LocationPOJO.java
@@ -8,7 +8,69 @@ import android.os.Parcelable;
  */
 
 public class LocationPOJO implements Parcelable {
+
+    public String _id;
+    public String place_id;
+    public String place_name;
+    public String place_address;
+    public String place_phone;
+    public String place_website;
+    public String place_latitude;
+    public String place_longitude;
+    public String place_rating;
+    public String place_image;
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public void setPlace_id(String place_id) {
+        this.place_id = place_id;
+    }
+
+    public void setPlace_name(String place_name) {
+        this.place_name = place_name;
+    }
+
+    public void setPlace_address(String place_address) {
+        this.place_address = place_address;
+    }
+
+    public void setPlace_phone(String place_phone) {
+        this.place_phone = place_phone;
+    }
+
+    public void setPlace_website(String place_website) {
+        this.place_website = place_website;
+    }
+
+    public void setPlace_latitude(String place_latitude) {
+        this.place_latitude = place_latitude;
+    }
+
+    public void setPlace_longitude(String place_longitude) {
+        this.place_longitude = place_longitude;
+    }
+
+    public void setPlace_rating(String place_rating) {
+        this.place_rating = place_rating;
+    }
+
+    public void setPlace_image(String place_image) {
+        this.place_image = place_image;
+    }
+
     protected LocationPOJO(Parcel in) {
+        _id = in.readString();
+        place_id = in.readString();
+        place_name = in.readString();
+        place_address = in.readString();
+        place_phone = in.readString();
+        place_website = in.readString();
+        place_latitude = in.readString();
+        place_longitude = in.readString();
+        //place_image = in.readByteArray();
+
     }
 
     public static final Creator<LocationPOJO> CREATOR = new Creator<LocationPOJO>() {
@@ -30,5 +92,14 @@ public class LocationPOJO implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(_id);
+        parcel.writeString(place_id);
+        parcel.writeString(place_name);
+        parcel.writeString(place_address);
+        parcel.writeString(place_phone);
+        parcel.writeString(place_website);
+        parcel.writeString(place_latitude);
+        parcel.writeString(place_longitude);
+        //parcel.writeString(_id);
     }
 }

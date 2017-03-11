151afed6d218c3ead6faea70c90a71b0e2ee6181
rajatsaini0294 <rajatkumar0294@gmail.com>
2017-03-11 21:00:39 +0530

    added runtime permission to create dir (Android 6.0+)

--- app/src/main/java/com/rajat/android/aller/ui/activities/MainActivity.java
+++ app/src/main/java/com/rajat/android/aller/ui/activities/MainActivity.java
@@ -1,16 +1,27 @@
 package com.rajat.android.aller.ui.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.rajat.android.aller.R;
 import com.rajat.android.aller.adapters.CustomPagerAdapter;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+    File appDirectoryPath;
+    public final String folder = "Aller";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +38,119 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-
+        createAppDirectory(folder);
     }
 
+    private int createAppDirectory(String folderName) {
 
+        String state = Environment.getExternalStorageState();
+        if (!Environment.MEDIA_MOUNTED.equals(state)){
+            Log.d("myAppName", "Error: external storage is unavailable");
+            return 0;
+        }
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Log.d("myAppName", "Error: external storage is read only.");
+            return 0;
+        }
+        Log.d("myAppName", "External storage is not read only or unavailable");
 
+        if (ContextCompat.checkSelfPermission(this, // request permission when it is not granted.
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("myAppName", "permission:WRITE_EXTERNAL_STORAGE: NOT granted!");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
 
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        if (!Environment.MEDIA_MOUNTED.equals(state)) {
+            Log.d("myAppName", "Error: external storage is unavailable");
+            return 0;
+        }
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Log.d("myAppName", "Error: external storage is read only.");
+            return 0;
+        }
+       // Log.d("myAppName", "External storage is not read only or unavailable");
+
+        File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Aller");
+        int result = 0;
+        if (folder.exists()) {
+            Log.d("myAppName", "folder exist:" + folder.toString());
+            result = 2; // folder exist
+        } else {
+            try {
+                if (folder.mkdir()) {
+                    Log.d("myAppName", "folder created:" + folder.toString());
+                    result = 1; // folder created
+                } else {
+                    Log.d("myAppName", "creat folder fails:" + folder.toString());
+                    result = 0; // creat folder fails
+                }
+            } catch (Exception ecp) {
+                ecp.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Aller");
+                    int result = 0;
+                    if (folder.exists()) {
+                        Log.d("............", "folder exist:" + folder.toString());
+                        result = 2; // folder exist
+                    } else {
+                        try {
+                            if (folder.mkdir()) {
+                                Log.d("................", "folder created:" + folder.toString());
+                                result = 1; // folder created
+                            } else {
+                                Log.d("..................", "creat folder fails:" + folder.toString());
+                                result = 0; // creat folder fails
+                            }
+                        } catch (Exception ecp) {
+                            ecp.printStackTrace();
+                        }
+                    }
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }

b2ae2c7bbf4545d1e0fbe9eb8822976f05164b37
rajatsaini0294 <rajatkumar0294@gmail.com>
2017-03-14 16:42:39 +0530

    Change constraint of column place_address

--- app/src/main/java/com/rajat/android/aller/data/TableColumns.java
+++ app/src/main/java/com/rajat/android/aller/data/TableColumns.java
@@ -16,7 +16,7 @@ public interface TableColumns {
     @DataType(INTEGER) @PrimaryKey @AutoIncrement String _ID = "_id";
     @DataType(TEXT) @NotNull String PLACE_ID = "place_id";
     @DataType(TEXT) String PLACE_NAME = "place_name";
-    @DataType(TEXT) @NotNull String PLACE_ADDRESS = "place_address";
+    @DataType(TEXT) String PLACE_ADDRESS = "place_address";
     @DataType(TEXT) String PLACE_PHONE = "place_phone";
     @DataType(TEXT) String PLACE_WEBSITE = "place_website";
     @DataType(TEXT) @NotNull String PLACE_LATITUDE = "place_latitude";

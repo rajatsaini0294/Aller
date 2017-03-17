1b8e3450229ba71744bd184ebc5d4ab66412c3bd
rajatsaini0294 <rajatkumar0294@gmail.com>
2017-03-17 12:23:56 +0530

    Add widgetprovider class

---
+++ app/src/main/java/com/rajat/android/aller/widget/WidgetProvider.java
@@ -0,0 +1,22 @@
+package com.rajat.android.aller.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by rajat on 3/17/2017.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
